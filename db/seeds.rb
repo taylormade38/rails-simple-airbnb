aeb239fdc652a69c3b66ad0bf67e943831473c77
Taylor Fong <taylor.fong.2012@gmail.com>
2020-06-25 10:19:10 +1000

    completed challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating restaurants..."
+5.times do
+   Flat.create!(
+      name: Faker::Hipster.sentence,
+      address: Faker::Address.street_name,
+      description: Faker::Hipster.sentences(number: 2),
+      price_per_night: rand(80..120),
+      number_of_guests: rand(1..5)
+    )
+end
+
+puts "Finished!"
